68af538788274960dfb61643e1308cc8a1e842a9
multidest <multidest@gmail.com>
2013-11-23 01:43:43 -0800

    swapping files

--- Health/admin.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'sarfarazhaque'
-
-from django.contrib import admin
-from Health.models import User
-
-admin.site.register(User)
-
-
--- Health/views.py
+++
@@ -1,7 +0,0 @@
-# Create your views here.
-
-
-from django.http import HttpResponse
-
-def index (Request):
-    return HttpResponse("This is the login page for HealthTrack")
